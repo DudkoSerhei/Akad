8295ea9e73b64a5ffdba60e804f4b0bebef1e7b1
DudkoSerhei <serhei.dudko@gmail.com>
2017-10-08 00:23:53 +0300

    Fix perfect styles

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
          styles:[{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
        });
 
-       var markerObject = new google.maps.Marker({
+       var marker = new google.maps.Marker({
          title: 'ПИН',
          position: new google.maps.LatLng(41.3947688, 2.0787279),
          map: Map,
@@ -47,7 +47,7 @@ $(".blog-pagination a").click(function() {
 });
 
 /*swiper slider*/
-var swiper = new Swiper ('.staff-item__container', {
+var swiper = new Swiper('.staff-item__container', {
     effect: 'flip',
     grabCursor: true,
     pagination:'.swiper-pagination',
